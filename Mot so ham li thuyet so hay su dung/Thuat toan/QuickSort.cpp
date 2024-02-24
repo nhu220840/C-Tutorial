1d31acaa30e8e08ccfc7ac3bc5eb232c7a5e1a44
nhu220840 <nhu220840@gmail.com>
2024-02-24 09:44:20 +0700

    update file

--- Mot so ham li thuyet so hay su dung/Thuat toan/QuickSort.cpp
+++ Mot so ham li thuyet so hay su dung/Thuat toan/QuickSort.cpp
@@ -35,7 +35,7 @@ int main(){
 	int n; scanf("%d", &n);
 	int a[n];
 	for(int i = 0; i < n; i++){
-		scanf("%d", &a[i]);
+		scanf("%d ", &a[i]);
 	}
 	quickSort(a, 0, n - 1);
 	for(int i = 0; i < n; i++){
