2e872152f749e228967eaf92a2d50f80e1d5254e
nhu220840 <nhu220840@gmail.com>
2024-03-19 13:21:50 +0700

    Add hoare partition

--- Mot so ham li thuyet so hay su dung/Thuat toan/QuickSort.cpp
+++ Mot so ham li thuyet so hay su dung/Thuat toan/QuickSort.cpp
@@ -8,36 +8,64 @@ void swap(int *a, int *b){
 	*b = tmp;
 }
 
-int partition(int a[], int l, int r){
-	int i = l - 1, pivot = a[r];
-	for(int j = l; j < r; j++){
+int lomutoPartition(int a[], int l, int r){
+	int pos = l - 1, pivot = a[r];
+	for(int i = l; i < r; i++){
 		//neu a[j] > pivot: bo qua
-		if(a[j] <= pivot){
+		if(a[i] <= pivot){
 			//dua a[j] len dau 
-			i++;
-			swap(&a[i], &a[j]);
+			pos++;
+			swap(&a[pos], &a[i]);
 		}
 	}
-	i++;
-	swap(&a[i], &a[r]);
-	return i;
+	pos++;
+	swap(&a[pos], &a[r]);
+	return pos;
+}
+
+//Hoare: cung luc tim 1 cap nghich the va doi cho chung cho nhau
+
+int hoarePartition(int a[], int l, int r){
+	int pivot = a[l];
+	int i = l - 1, j = r + 1;
+	while(true){
+		do{
+			i++;
+		}while(a[i] < pivot); //tim phan tu ben trai >= pivot
+
+		do{
+			j--;
+		}while(a[j] > pivot); //tim phan tu ben phai <= pivot
+		
+		if(i < j){
+			swap(&a[i], &a[j]); //doi cho 2 phan tu cho nhau
+		}
+		else return j; //tra ve vi tri phan chia
+	}
 }
 
 void quickSort(int a[], int l, int r){
 	if(l < r){
-		int pos = partition(a, l, r);
+		int pos = lomutoPartition(a, l, r);
 		quickSort(a, l, pos - 1); //xet de quy ve ben trai
 		quickSort(a, pos + 1, r); //xet de quy ve ban phai
 	}
+}
+
+void quickSort2(int a[], int l, int r){
+	if(l >= r) return;
+	int pos = hoarePartition(a, l, r);
+	quickSort2(a, l, pos);
+	quickSort2(a, pos + 1, r);
 }
 
 int main(){
 	int n; scanf("%d", &n);
 	int a[n];
 	for(int i = 0; i < n; i++){
-		scanf("%d ", &a[i]);
+		scanf("%d", &a[i]);
 	}
-	quickSort(a, 0, n - 1);
+	quickSort2(a, 0, n - 1);
 	for(int i = 0; i < n; i++){
 		printf("%d ", a[i]);
 	}
